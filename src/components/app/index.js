ab43347389dd24b81d68dae61e9a5147284f1b54
Cameron Sampson <csampson.js@gmail.com>
2016-12-19 16:50:48 -0800

    Moved `<app>` to separate directory

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,7 +4,12 @@
  * @requires @angular/core/Component
  * @requires @angular/http/HTTP_PROVIDERS
  * @requires @angular/platform-browser-dynamic
+ * @requires rxjs/add/observable/throw
+ * @requires rxjs/add/operator/catch
+ * @requires rxjs/add/operator/debounceTime
+*  @requires rxjs/add/operator/map
  * @requires player
+ * @requires styles
  */
 
 import { Component }      from '@angular/core';
@@ -16,13 +21,16 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
 
-import { Player } from './components';
+import { Player } from '..';
+import styles     from './styles.css';
+import template   from './template.html';
 
 @Component({
   selector: 'app',
-  template: `<player></player>`,
   directives: [Player],
-  providers: [HTTP_PROVIDERS]
+  providers: [HTTP_PROVIDERS],
+  styles: [styles],
+  template
 })
 class AppComponent {}
 
